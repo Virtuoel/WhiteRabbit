a896d9076fbd4ad3a0bc4fec3371421ce8726a29
Virtuoel <Virtuoel@users.noreply.github.com>
2021-03-19 01:23:40 +0300

    Tweak id util method to allow for path varargs

--- src/main/java/virtuoel/white_rabbit/WhiteRabbit.java
+++ src/main/java/virtuoel/white_rabbit/WhiteRabbit.java
@@ -151,9 +151,14 @@ public class WhiteRabbit implements ModInitializer
 		});
 	}
 	
-	public static Identifier id(String name)
+	public static Identifier id(String path)
 	{
-		return new Identifier(MOD_ID, name);
+		return new Identifier(MOD_ID, path);
+	}
+	
+	public static Identifier id(String path, String... paths)
+	{
+		return id(paths.length == 0 ? path : path + String.join("/", paths));
 	}
 	
 	public static int getShrinkDelayTicks(ScaleData scaleData)
