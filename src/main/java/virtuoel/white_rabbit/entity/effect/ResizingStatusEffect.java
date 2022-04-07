a9722165cdd7a3a1511633ccd58ac99b022ddfa1
Virtuoel <Virtuoel@users.noreply.github.com>
2022-04-07 21:35:51 +0300

    22w14a

--- src/main/java/virtuoel/white_rabbit/entity/effect/ResizingStatusEffect.java
+++ src/main/java/virtuoel/white_rabbit/entity/effect/ResizingStatusEffect.java
@@ -5,7 +5,6 @@ import net.minecraft.entity.attribute.AttributeContainer;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.math.Vec3d;
 import virtuoel.pehkui.api.ScaleData;
 import virtuoel.pehkui.api.ScaleType;
 import virtuoel.white_rabbit.init.ScaleTypeRegistrar;
@@ -46,12 +45,7 @@ public class ResizingStatusEffect extends StatusEffect
 	{
 		super.onApplied(entity, attributes, amplifier);
 		
-		final Vec3d pos = entity.getPos();
-		
-		if (!entity.isSilent())
-		{
-			entity.world.playSound(null, pos.x, pos.y, pos.z, SoundEvents.ENTITY_EVOKER_CAST_SPELL, entity.getSoundCategory(), 1.0F, 1.0F);
-		}
+		entity.playSound(SoundEvents.ENTITY_EVOKER_CAST_SPELL, 1.0F, 1.0F);
 	}
 	
 	@Override
@@ -59,12 +53,7 @@ public class ResizingStatusEffect extends StatusEffect
 	{
 		super.onRemoved(entity, attributes, amplifier);
 		
-		final Vec3d pos = entity.getPos();
-		
-		if (!entity.isSilent())
-		{
-			entity.world.playSound(null, pos.x, pos.y, pos.z, SoundEvents.ENTITY_ZOMBIE_VILLAGER_CONVERTED, entity.getSoundCategory(), 1.0F, 1.0F);
-		}
+		entity.playSound(SoundEvents.ENTITY_ZOMBIE_VILLAGER_CONVERTED, 1.0F, 1.0F);
 		
 		final ScaleType type = ScaleTypeRegistrar.FOOD_TYPE;
 		
