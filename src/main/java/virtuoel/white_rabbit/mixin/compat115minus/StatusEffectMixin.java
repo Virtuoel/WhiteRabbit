9614969af527147b545ecd9b6c31c4ec7a21fbb6
Virtuoel <Virtuoel@users.noreply.github.com>
2022-04-13 17:31:59 +0300

    Fix application/removal effects not working in 1.15.2 and below

---
+++ src/main/java/virtuoel/white_rabbit/mixin/compat115minus/StatusEffectMixin.java
@@ -0,0 +1,35 @@
+package virtuoel.white_rabbit.mixin.compat115minus;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Coerce;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import virtuoel.white_rabbit.entity.effect.ResizingStatusEffect;
+import virtuoel.white_rabbit.init.StatusEffectRegistrar;
+import virtuoel.white_rabbit.util.MixinConstants;
+
+@Mixin(StatusEffect.class)
+public class StatusEffectMixin
+{
+	@Inject(at = @At("HEAD"), method = MixinConstants.ON_APPLIED, remap = false)
+	private void onOnApplied(LivingEntity entity, @Coerce Object attributes, int amplifier, CallbackInfo info)
+	{
+		if ((Object) this == StatusEffectRegistrar.RESIZING)
+		{
+			ResizingStatusEffect.doApplication(entity);
+		}
+	}
+	
+	@Inject(at = @At("HEAD"), method = MixinConstants.ON_REMOVED, remap = false)
+	private void onOnRemoved(LivingEntity entity, @Coerce Object attributes, int amplifier, CallbackInfo info)
+	{
+		if ((Object) this == StatusEffectRegistrar.RESIZING)
+		{
+			ResizingStatusEffect.doRemoval(entity);
+		}
+	}
+}
---
+++ src/main/java/virtuoel/white_rabbit/util/MixinConstants.java
@@ -0,0 +1,10 @@
+package virtuoel.white_rabbit.util;
+
+public class MixinConstants
+{
+	private static final String METHOD_5555 = "method_5555";
+	private static final String METHOD_5562 = "method_5562";
+	
+	public static final String ON_APPLIED = METHOD_5555;
+	public static final String ON_REMOVED = METHOD_5562;
+}
